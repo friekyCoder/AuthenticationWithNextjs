fa9197e2ab78053845f742a5b36a5edcf5941dcc
harshit <harshit302001n@gmail.com>
2023-10-31 03:33:55 +0530

    authentication done

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectMongo from "@/database/conn";
+import Users from "@/model/Schema";
+import { compare } from "bcryptjs";
+//import EmailProvider from "next-auth/providers/email";
+
+export default NextAuth({
+  providers: [
+    // OAuth authentication providers...
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials, req) {
+        connectMongo().catch((error) => {
+          error: "Connection Failed...!";
+        });
+
+        // check user existance
+        const result = await Users.findOne({ email: credentials.email });
+        if (!result) {
+          throw new Error("No user Found with Email Please Sign Up...!");
+        }
+
+        // compare()
+        const checkPassword = await compare(
+          credentials.password,
+          result.password
+        );
+
+        // incorrect password
+        if (!checkPassword || result.email !== credentials.email) {
+          throw new Error("Username or Password doesn't match");
+        }
+
+        return result;
+      },
+    }),
+  ],
+
+  /* adapter: MongoDBAdapter(clientPromise), */
+});
